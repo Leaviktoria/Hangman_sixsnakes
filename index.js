223e48a73e1e2e99bb76d8157036a55ac9a45e88
Viktoria Bzdyk <viktoria.bzdyk@gmx.at>
2022-11-12 15:33:43 +0100

    change

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const { WORDS_TO_GUESS_1, HANGMAN_PICS } = require('./constants');
+const { WORDS_TO_GUESS_1 } = require('./constants');
 const { WORDS_TO_GUESS_2 } = require('./constants');
 const { WORDS_TO_GUESS_3 } = require('./constants');
 const constants = require('./constants');
 let alphaBet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-let randomWord = ""
+
 // In node.js: install a prompt library by running: `npm install prompt-sync` in the current folder
 const prompt = require("prompt-sync")();
 
@@ -15,18 +15,15 @@ Das Ziel ist es, das gegebene Wort zu erraten. Bei den Wörtern handelt es sich 
 let chooseLevel = prompt(`Welches Level von 1-3 willst du spielen?`);
 
 if (chooseLevel = `1`) {
-randomWord = WORDS_TO_GUESS_1[Math.floor(Math.random()*myArray.length)];
-
+WORDS_TO_GUESS = WORDS_TO_GUESS_1;
 };
 
 if (chooseLevel = `2`) {
-    randomWord = WORDS_TO_GUESS_2[Math.floor(Math.random()*myArray.length)];
-    console.log(HANGMAN_PICS[1]);
+    WORDS_TO_GUESS = WORDS_TO_GUESS_2;
 };
 
 if (chooseLevel = `3`) {
-    randomWord = WORDS_TO_GUESS_3[Math.floor(Math.random()*myArray.length)];
-    console.log(HANGMAN_PICS[3]);
+    WORDS_TO_GUESS = WORDS_TO_GUESS_3;
 };
 
 //Figure 
@@ -106,16 +103,3 @@ while (true) {          // main loop
  if (answer == `q`) {
      ProcessingInstruction.exit(0)
  } 
-
-
-
-
-
-
-
-
-
-
- var randomItem = myArray[Math.floor(Math.random()*myArray.length)];
-
-  console.log(Array.from(randomItem))
