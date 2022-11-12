20954d874c0e2019475572df0ef3799734c622ad
Viktoria Bzdyk <viktoria.bzdyk@gmx.at>
2022-11-12 13:21:32 +0100

    Einleitung

--- index.js
+++ index.js
@@ -2,6 +2,32 @@ const constants = require('./constants');
 // In node.js: install a prompt library by running: `npm install prompt-sync` in the current folder
 const prompt = require("prompt-sync")();
 
+
+console.log(`Welcome to Hangman.
+Das Ziel ist es, das gegebene Wort zu erraten. Bei den Wörtern handelt es sich um Arten von Schlangen.`)
+
+let chooseLevel = prompt(`Welches Level von 1-3 willst du spielen?`);
+
+if chooseLevel = `1` {
+
+
+};
+
+
+if chooseLevel = `2` {
+
+
+
+};
+
+if chooseLevel = `3` {
+
+
+
+};
+
+
+
 // Here you see an example how to get your
 // constants from constants.js
 // for(let figure of constants.HANGMAN_PICS)
