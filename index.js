7d5ec1d4315033b2002cfe0fd4c5e5c0d930e449
Viktoria Bzdyk <viktoria.bzdyk@gmx.at>
2022-11-12 15:08:04 +0100

    changes

--- index.js
+++ index.js
@@ -29,7 +29,6 @@ for(let figure of constants.HANGMAN_PICS)
  {  console.log(figure);
  };
 
-<<<<<<< HEAD
 
  
 let positiveAttrs = [];
@@ -100,8 +99,6 @@ while (true) {          // main loop
         process.exit(0);
     }
 }
-=======
  if (answer == `q`) {
      ProcessingInstruction.exit(0)
  } 
->>>>>>> 6035b968b6f9e8879ad618a7e841fefd6ed73752
