295f736c8de2255d5adbab6f13620c35e2bebc8c
Viktoria Bzdyk <viktoria.bzdyk@gmx.at>
2022-11-12 14:48:08 +0100

    corrections

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { WORDS_TO_GUESS } = require('./constants');
 const constants = require('./constants');
 // In node.js: install a prompt library by running: `npm install prompt-sync` in the current folder
 const prompt = require("prompt-sync")();
@@ -6,35 +7,22 @@ const prompt = require("prompt-sync")();
 console.log(`Welcome to Hangman.
 Das Ziel ist es, das gegebene Wort zu erraten. Bei den Wörtern handelt es sich um Arten von Schlangen.`)
 
+//Level
 let chooseLevel = prompt(`Welches Level von 1-3 willst du spielen?`);
 
 if (chooseLevel = `1`) {
-
-
+WORDS_TO_GUESS = WORDS_TO_GUESS_1;
 };
 
-
 if (chooseLevel = `2`) {
-
-
-
+    WORDS_TO_GUESS = WORDS_TO_GUESS_2;
 };
 
 if (chooseLevel = `3`) {
-
-
-
+    WORDS_TO_GUESS = WORDS_TO_GUESS_3;
 };
 
 
-
-// Here you see an example how to get your
-// constants from constants.js
-// for(let figure of constants.HANGMAN_PICS)
-// {
-//     console.log(figure);
-// }
-
-
-// how to use the prompt - e.g.:
-// const name = prompt('What is your name?');
+for(let figure of constants.HANGMAN_PICS)
+ {  console.log(figure);
+ };
