e5213560b7258e131d5d1b7c2e3a9a45027c4a29
Leaviktoria <l.moerkenstein@gmail.com>
2022-11-15 19:06:37 +0100

    constants

--- constants.js
+++ constants.js
@@ -52,9 +52,9 @@ const HANGMAN_PICS = [
 
 // TODO: Fill this list with values about a certain topic
 // you are passionate about: e.g. famous scientists, chess players, ...
-const WORDS_TO_GUESS_1 = `Boa`, `Viper`, `Otter`,`Kobra`,`Mamba`;
-const WORDS_TO_GUESS_2 = `Natter`, `Python`, `Aeskula`,`Anakonda`, `Kornnatter`;
-const WORDS_TO_GUESS_3 = `Warzenschlange`, `Blindschlange`, `Kupferkopf`, `Seeschlange`,`Klapperschlange`;
+const WORDS_TO_GUESS_1 = ["Boa", "Viper", "Otter","Kobra","Mamba"];
+const WORDS_TO_GUESS_2 = ["Natter", "Python", "Aeskula", "Anakonda", "Kornnatter"];
+const WORDS_TO_GUESS_3 = ["Warzenschlange", "Blindschlange", "Kupferkopf", "Seeschlange","Klapperschlange"];
 
 module.exports = {
   HANGMAN_PICS: HANGMAN_PICS,
